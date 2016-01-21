6effb15084f447e0ae43d2a9f02441154efe7798
Alex Fallenstedt and Hillary Hintzen <hillary07@gmail.com>
2016-01-21 09:22:51 -0800

    set up initial files and two specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+var countBy = function(){
+  if (countBy = 2);
+  return countBy;
+}
+var countTo = function(){
+  if (countTo = 10);
+  return countTo;
+}
+
+
 $(document).ready(function() {
-  
+
 });
---
+++ specs/specs.js
@@ -0,0 +1,11 @@
+describe("countBy", function(){
+  it("establish a positive or negative number for our program to count by", function(){
+    expect(countBy(2)).to.equal(2)
+  });
+})
+describe("countTo", function(){
+  it("establish a positive or negative number for the porgram to count to", function(){
+    expect(countTo(10)).to.equal(10)
+  });
+});
+describe("")
